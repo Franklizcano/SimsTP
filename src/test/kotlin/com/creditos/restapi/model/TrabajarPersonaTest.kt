5cc3b3de0687b023f3d82af061f3db3700882424
Frank Lizcano <franklizcano96@gmail.com>
2021-06-28 13:31:34 -0300

    Implementando el endpoint para trabajar y su persistencia

--- src/test/kotlin/com/creditos/restapi/model/TrabajarPersonaTest.kt
+++ src/test/kotlin/com/creditos/restapi/model/TrabajarPersonaTest.kt
@@ -35,14 +35,15 @@ class TrabajarPersonaTest {
                 LocalDate.parse("25-06-1999", formatter))
 
             tipoProfesionStrategy.trabajar(persona)
-            println("-------")
+            println("${persona.nombre} está trabajando como ${persona.profesion}")
 
             val persona2 = persona.copy(profesion = TipoProfesionEnum.Medico)
             tipoProfesionStrategy.trabajar(persona2)
-            println("-------")
+            println("${persona2.nombre} está trabajando como ${persona2.profesion}")
 
             val persona3 = persona.copy(profesion = TipoProfesionEnum.Ingeniero)
             tipoProfesionStrategy.trabajar(persona3)
+            println("${persona3.nombre} está trabajando como ${persona3.profesion}")
         }
     }
 
